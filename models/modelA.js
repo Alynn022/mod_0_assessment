f03185dc692ff06964b0d89e316e9b0a2cfe20eb
Lynn Nguyen <AmandaN22@outlook.com>
2021-09-24 11:48:34 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+let dinosaur = {
+  name: 'T-Rex',
+  size: 'big',
+  limbs: 4,
+  lifeSpan: 30,
+  isCarnivore: true,
+  isPredator: true,
+  foundIn: ['Montana', 'South Dakota', 'Alberta, Canada'],
+  favoriteFoods: ['Triceratops', 'Corythosaurus', 'Edmontosaurus']
+};
