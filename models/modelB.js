05dbc0c7a3301b6332a87f747c198322f2b8c769
Lynn Nguyen <AmandaN22@outlook.com>
2021-09-24 11:56:53 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  computer: 'Mac',
+  school: 'Turing School of Software and Design',
+  programInMonths: 7,
+  tuitionCost: 20000,
+  fullTime: true,
+  isStudent: true,
+  types: ['Front End', 'Back End'],
+  toolsUsed: ['chrome', 'atom', 'terminal'],
+};
